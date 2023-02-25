28113a253fb885bc03f47f613c8a3f906fa1aa2e
LiorAharon98 <lior.ah98@gmail.com>
2023-02-25 23:31:57 +0200

    update1

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserRoute = require("./routes/UserRoute");
 const LotteryRoute = require("./routes/LotteryRoute");
-// const createLottery = require("./createLottery");
+const createLottery = require("./createLottery");
 const app = express();
 const serverConfig = require("./config");
-console.log("sd")
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URI ||"mongodb+srv://liors-database:lior.ah980@cluster0.iybrzvm.mongodb.net/lottery?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URI);
 serverConfig(app, express);
 app.use("/lottery", UserRoute);
 app.use("/lottery", LotteryRoute);
-// const handleCreateLottery = async () => {
-//   await createLottery();
-// };
-// handleCreateLottery();
-app.listen( process.env.PORT || 8000);
+const handleCreateLottery = async () => {
+  await createLottery();
+};
+handleCreateLottery();
+app.listen(process.env.PORT);
