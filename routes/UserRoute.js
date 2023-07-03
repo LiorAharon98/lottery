11fd110f458fb86bd46677dcbb47aa72bda081cc
LiorAharon98 <lior.ah98@gmail.com>
2023-07-03 16:56:05 +0300

    added new features && improve perfomance

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -8,7 +8,6 @@ router.post("/sign-up", async (req, res) => {
   const existUser = await UserModal.findOne({ username });
   if (existUser) return res.json(false);
   const user = await UserModal.create(req.body);
-
   res.json(user);
 });
 
@@ -18,7 +17,8 @@ router.post("/sign-in", async (req, res) => {
   if (user) {
     const auth = await bcrypt.compare(password, user.password);
     if (auth) {
-      return res.json(user);
+      const userWithoutPassword = await UserModal.findOne({ username }, "-password");
+      return res.json(userWithoutPassword);
     }
   }
   return res.json(null);
@@ -30,7 +30,7 @@ router.put("/create-user-lottery-numbers", async (req, res) => {
     { $set: { lotteryNumbers: { firstColumn: arr[0], secondColumn: arr[1] } } },
     { new: true }
   );
-  res.json(user);
+  res.json(user.lotteryNumbers);
 });
 router.put("/lottery-page", async (req, res) => {
   const { username, number, date } = req.body;
@@ -39,25 +39,24 @@ router.put("/lottery-page", async (req, res) => {
     { $inc: { earned: number }, $set: { latestWin: { number, date } } },
     { new: true }
   );
-  res.json(user);
+  res.json({ earned: user.earned, latestWin: user.latestWin });
 });
 router.put("/user-setting", async (req, res) => {
   const { username, profilePicture } = req.body;
   const user = await UserModal.findOneAndUpdate({ username }, { profilePicture }, { new: true });
-  res.json(user);
+  res.json(user.profilePicture);
 });
 
-router.put("/user/bank" , async(req,res)=>{
-const {username} =req.body
-const user = await UserModal.findOneAndUpdate({username}, {$set : {bank : true}},{new : true})
-res.json(user)
-})
+router.put("/user/bank", async (req, res) => {
+  const { username } = req.body;
+  const user = await UserModal.findOneAndUpdate({ username }, { $set: { bank: true } }, { new: true });
+  res.json(user.bank);
+});
 router.post("/user-setting", async (req, res) => {
   const { username, passwordToChange } = req.body;
   const salt = await bcrypt.genSalt();
   const newPassword = await bcrypt.hash(passwordToChange, salt);
-  const user = await UserModal.findOneAndUpdate({ username }, {password : newPassword }, { new: true });
-  res.json(user);
+  await UserModal.findOneAndUpdate({ username }, { password: newPassword }, { new: true });
 });
 
 module.exports = router;
