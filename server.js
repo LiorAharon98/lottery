2616e37b640b30d68279bfd67bf31276494589ba
LiorAharon98 <lior.ah98@gmail.com>
2025-02-25 18:01:17 +0200

    add new version

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const UserRoute = require("./routes/UserRoute");
@@ -5,8 +6,11 @@ const LotteryRoute = require("./routes/LotteryRoute");
 const app = express();
 const serverConfig = require("./config");
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  (process.env.NODE = "development" ? process.env.LOCAL_MONGODB_URI : process.env.PRODUCTION_MONGODB_URI)
+);
 serverConfig(app, express);
 app.use("/lottery", UserRoute);
 app.use("/lottery", LotteryRoute);
 app.listen(process.env.PORT);
+
